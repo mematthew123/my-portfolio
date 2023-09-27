a7ab9f4f144cfa291d0848a22eb8e68938011e6b
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-09-27 14:02:51 -0600

    removed gradient and added bg blur to hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,16 @@
-import Header from "@/components/Header";
-import "./globals.css";
-import { Inter } from "next/font/google";
-import Footer from "@/components/Footer";
-import ActiveSectionContextProvider from "@/context/active-section-context";
-import ThemeContextProvider from "@/context/ThemeContext";
-import ThemeSwitch from "@/components/ThemeSwitch";
+import Header from '@/components/Header';
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Footer from '@/components/Footer';
+import ActiveSectionContextProvider from '@/context/active-section-context';
+import ThemeContextProvider from '@/context/ThemeContext';
+import ThemeSwitch from '@/components/ThemeSwitch';
 
-
-
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Matthew Rhoads | Portfolio",
-  description: "Software Engineer",
+  title: 'Matthew Rhoads | Portfolio',
+  description: 'Software Engineer',
 };
 
 export default function RootLayout({
@@ -21,10 +19,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" >
+    <html lang='en'>
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+        className={`${inter.className} bg-gray-200 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
+        <div className='bg-[#e2e8fb] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-emerald-700'></div>
+        <div className='bg-[#b2b9e46c] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#4d787711]'></div>
+
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
             <Header />
